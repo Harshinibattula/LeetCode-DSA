699deba81af94ceda06e614b47c317b23c20d40d
Harshinibattula <86304216+Harshinibattula@users.noreply.github.com>
2025-07-22 08:36:45 +0530

    Time: 1 ms (66.03%) | Memory: 13.1 MB (61.27%) - LeetSync

--- 1-two-sum/two-sum.py
+++ 1-two-sum/two-sum.py
@@ -1,3 +1,12 @@
+#O(n^2)---brute force 
+# class Solution:
+#     def twoSum(self, nums, target):
+#         for i in range(len(nums)):
+#             x = target - nums[i]
+#             for j in range(i + 1, len(nums)):
+#                 if nums[j] == x:
+#                     return [i, j]
+
 #O(n)
 class Solution:
     def twoSum(self,nums,target):
